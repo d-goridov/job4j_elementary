7e352af8a1cb13e5e5fc9be3cdcce8546495d215
d.goridov <dimagoridov@yandex.ru>
2022-02-14 12:54:06 +0300

    5. Конвертер валюты [#41583]  Довавил 2 метода конвертации euroToDollar() и dollarToEuro().  Доработал метод main. Добавил в него примеры выводов всех  видов конвертаций.

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,34 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+
+    public static float rubleToDollar(float value) {
+        return value / 60;
+    }
+
+    public static float euroToDollar(int value) {
+        return value * 1.16f;
+    }
+
+    public static float dollarToEuro(int value) {
+        return value * 0.85f;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        float dollar = Converter.rubleToDollar(245);
+        String result = String.format("%.2f", dollar);
+        System.out.println("245 rubles are " + result + " dollar.");
+        float dollar1 = Converter.euroToDollar(10);
+        String result2 = String.format("%.2f", dollar1);
+        System.out.println("10 euro are " + result2 + " dollar.");
+        float euro2 = Converter.dollarToEuro(21);
+        String result3 = String.format("%.1f", euro2);
+        System.out.println("21 dollar are " + result3 + " euro.");
+    }
+}
