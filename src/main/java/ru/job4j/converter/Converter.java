542bf9d5698da920a541c3e8f1506e7d112322da
d.goridov <dimagoridov@yandex.ru>
2022-02-14 20:44:26 +0300

    5. Конвертер валюты [#41583 #264422] Правок добавлено не было, сделал еще один коммит, т.к. бот не пропустил из-за однострочного комментария в классе MathFunc

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -21,12 +21,15 @@ public class Converter {
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
+
         float dollar = Converter.rubleToDollar(245);
         String result = String.format("%.2f", dollar);
         System.out.println("245 rubles are " + result + " dollar.");
+
         float dollar1 = Converter.euroToDollar(10);
         String result2 = String.format("%.2f", dollar1);
         System.out.println("10 euro are " + result2 + " dollar.");
+
         float euro2 = Converter.dollarToEuro(21);
         String result3 = String.format("%.1f", euro2);
         System.out.println("21 dollar are " + result3 + " euro.");
