69b407bdff60d6900567fdd3485f6068c15c0cd4
d.goridov <dimagoridov@yandex.ru>
2022-02-15 21:36:14 +0300

    0. Что такое тестирование [#159431]  Доработал код в методе main класса ru.job4j.converter.Converter.  Добавил проверки методов

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -10,11 +10,11 @@ public class Converter {
         return value / 60;
     }
 
-    public static float euroToDollar(int value) {
+    public static float euroToDollar(float value) {
         return value * 1.16f;
     }
 
-    public static float dollarToEuro(int value) {
+    public static float dollarToEuro(float value) {
         return value * 0.85f;
     }
 
@@ -22,7 +22,7 @@ public class Converter {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
 
-        float dollar = Converter.rubleToDollar(245);
+        float dollar = Converter.rubleToDollar(240);
         String result = String.format("%.2f", dollar);
         System.out.println("245 rubles are " + result + " dollar.");
 
@@ -33,5 +33,28 @@ public class Converter {
         float euro2 = Converter.dollarToEuro(21);
         String result3 = String.format("%.1f", euro2);
         System.out.println("21 dollar are " + result3 + " euro.");
+
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed);
+
+        in = 240;
+        expected = 4;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("240 rubles are 4 dollar. Test result : " + passed);
+
+        in = 100;
+        expected = 116;
+        out = Converter.euroToDollar(in);
+        passed = expected == out;
+        System.out.println("100 euro are 116 dollars. Test result : " + passed);
+
+        expected = 85;
+        out = Converter.dollarToEuro(in);
+        passed = expected == out;
+        System.out.println("100 dollars are 85 euro. Test result : " + passed);
     }
 }
