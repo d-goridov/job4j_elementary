3b5b8cb45c39a8051d4becaab45e837c93904e52
d.goridov <dimagoridov@yandex.ru>
2022-02-21 21:31:20 +0300

    6.0.5. Массивы в тестах. [#226789] Добавил тест-метод для проверки bound = 5.

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -1,7 +1,7 @@
 package ru.job4j.array;
 
 public class Square {
-    @SuppressWarnings("checkstyle:WhitespaceAfter")
+
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int index = 0; index < bound; index++) {
