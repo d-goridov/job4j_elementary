069ad0433150c7c5f2e61b142eb1002b84e5b0d9
d.goridov <dimagoridov@yandex.ru>
2022-02-21 21:45:02 +0300

    6.1. Классический поиск перебором. [#33489] Реализовал метод поиска элемента в массиве. Добавил два тест-метода.

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = new int[]{5, 4, 3, 2};
+        int element = 5;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = new int[]{5, 3, 3, -4, 20};
+        int element = 10;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas21Then3() {
+        int[] data = new int[]{0, -98, 15, 21, 3};
+        int element = 21;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas0ThenMinus1() {
+        int[] data = new int[]{53, -54, 13, 2, 11};
+        int element = 0;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+}
