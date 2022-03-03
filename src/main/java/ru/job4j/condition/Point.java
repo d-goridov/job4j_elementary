3d5cae0742b371c4d771dabdbe43d670d5cec580
d.goridov <dimagoridov@yandex.ru>
2022-03-03 20:24:27 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587] Рефакторинг класса Point в ООП парадигме.

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,18 +1,15 @@
 package ru.job4j.condition;
 
 public class Point {
+    private int x;
+    private int y;
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0): " + result);
-        double result1 = Point.distance(1, 3, 4, 2);
-        System.out.println("result (1, 3) to (4, 2): " + result1);
-        double result3 = Point.distance(0, 2, 1, 3);
-        System.out.println("result (0, 2) to (1, 3): " + result3);
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
     }
-
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -6,37 +6,30 @@ import org.junit.Test;
 public class PointTest {
 
     @Test
-    public void when01to20then2dot24() {
+    public void whenThis01Another20ThenDistance2dot24() {
+        Point point1 = new Point(0, 1);
+        Point point2 = new Point(2, 0);
+        double result = point1.distance(point2);
         double expected = 2.24;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, result, 0.01);
 
     }
 
     @Test
-    public void when11to33then2dot83() {
+    public void whenThis11Another33ThenDistance2dot83() {
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(3, 3);
+        double result = point1.distance(point2);
         double expected = 2.83;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 3;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, result, 0.01);
     }
 
     @Test
-    public void when20to51then3dot17() {
+    public void whenThis01Another20ThenDistance3dot17() {
+        Point point1 = new Point(2, 0);
+        Point point2 = new Point(5, 1);
+        double result = point1.distance(point2);
         double expected = 3.17;
-        int x1 = 2;
-        int y1 = 0;
-        int x2 = 5;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-
+        Assert.assertEquals(expected, result, 0.01);
     }
 }
