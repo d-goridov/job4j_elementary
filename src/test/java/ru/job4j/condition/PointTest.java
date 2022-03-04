f8592dcf9d4d0afdd62d3bd397be573a80b9190b
d.goridov <dimagoridov@yandex.ru>
2022-03-04 13:53:41 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643] Перегрузил конструктор Point. Реализовал метод distance3d. Тесты

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -25,11 +25,29 @@ public class PointTest {
     }
 
     @Test
-    public void whenThis01Another20ThenDistance3dot17() {
+    public void whenThis20Another51ThenDistance3dot17() {
         Point point1 = new Point(2, 0);
         Point point2 = new Point(5, 1);
         double result = point1.distance(point2);
         double expected = 3.17;
+        Assert.assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void whenThis013Another20Minus1ThenDistance4dot58() {
+        Point point1 = new Point(0, 1, 3);
+        Point point2 = new Point(2, 0, -1);
+        double result = point1.distance3d(point2);
+        double expected = 4.58;
+        Assert.assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void whenThis112Another203ThenDistance1dot73() {
+        Point point1 = new Point(1, 1, 2);
+        Point point2 = new Point(2, 0, 3);
+        double result = point1.distance3d(point2);
+        double expected = 1.73;
         Assert.assertEquals(expected, result, 0.01);
     }
 }
